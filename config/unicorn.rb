f877dc95ccfcc89b24ece7e7c113ebe84f75068c
wish911wish <wish911wish@gmail.com>
2019-03-06 19:47:13 +0900

    Edit config for deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -38,6 +38,3 @@ after_fork do |_server, _worker|
  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
 
-before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = @app_path + "/current/Gemfile"
-end
