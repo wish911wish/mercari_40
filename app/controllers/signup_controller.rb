c2c6c92106f292ad85173cad15f966c6b3ea6df4
wish911wish <wish911wish@gmail.com>
2019-03-16 17:41:46 +0900

    Implementation of user-signup-function

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -3,4 +3,28 @@ class SignupController < ApplicationController
   def index
   end
 
+  def sms_confirmation
+  end
+
+  def address_registration
+    current_user.update(user_params_phone_number)
+  end
+
+
+  def payment_registration
+    current_user.update(user_params_address)
+  end
+
+
+  def completion
+  end
+
+  private
+  def user_params_phone_number
+    params.require(:user).permit(:phone_number)
+  end
+
+  def user_params_address
+    params.require(:user).permit(:postal_code, :prefectures ,:city ,:street_address, :building)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'signup/index'
-
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root 'top#index'
   resources :items, only: [:index, :new]
-  resources :users, only: [:new, :sms_confirmation, :address_registration, :payment_registration, :completion]
-  post '/signup/sms_confirmation', to: 'users#sms_confirmation', as: 'sms_confirmation'
 
-  resources :signup, only: [:index]
+  get '/signup', to: 'signup#index', as: 'user_signup'
+  get '/signup/sms_confirmation', to: 'signup#sms_confirmation', as: 'sms_confirmation'
+  patch '/signup/address_registration', to: 'signup#address_registration', as: 'address_registration'
+  patch '/signup/payment_registration', to: 'signup#payment_registration', as: 'payment_registration'
+  get '/signup/completion', to: 'signup#completion', as: 'completion'
 
 end
