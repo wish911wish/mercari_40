1f53f0e508ab1c8fd4e62ade96a758ead7b7f944
wish911wish <wish911wish@gmail.com>
2019-03-17 11:29:19 +0900

    Delete unnecessary comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   before_save :convert_to_full_width_characters
