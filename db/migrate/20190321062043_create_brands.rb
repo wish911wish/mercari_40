5f12305060abe4b433d2059cda784febfc5d653e
wish911wish <wish911wish@gmail.com>
2019-03-21 15:27:36 +0900

    Create brands table

---
+++ db/migrate/20190321062043_create_brands.rb
@@ -0,0 +1,9 @@
+class CreateBrands < ActiveRecord::Migration[5.0]
+  def change
+    create_table :brands do |t|
+      t.string    :name, null: false
+      t.references   :category, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190321061647) do
+ActiveRecord::Schema.define(version: 20190321062043) do
+
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",        null: false
+    t.integer  "category_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_brands_on_category_id", using: :btree
+  end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
