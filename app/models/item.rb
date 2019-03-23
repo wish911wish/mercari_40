57d155889d455f75f480d09c7cc72fffdda3d583
wish911wish <wish911wish@gmail.com>
2019-03-23 16:40:35 +0900

    Definition of Association for Item and User

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,15 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: "seller_id"
   has_many :categories
-  has_many :item_images
   has_one :condition
   has_one :shipping_cost
   has_one :shipping_method
   has_one :days_for_shipment
   has_one :status
+
+  with_options presence: true do
+    validates :name
+    validates :description
+  end
+
 end
