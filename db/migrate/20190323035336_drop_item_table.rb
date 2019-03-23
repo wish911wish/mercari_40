b8a9af0b69e83aca5dc64b0aa56c5090005c0201
wish911wish <wish911wish@gmail.com>
2019-03-23 17:37:31 +0900

    Edit migrate file

--- db/migrate/20190323035336_drop_item_table.rb
+++ db/migrate/20190323035336_drop_item_table.rb
@@ -2,9 +2,6 @@ class DropItemTable < ActiveRecord::Migration[5.0]
   def change
     remove_foreign_key :items, :users
     remove_reference :items, :big_category
-    remove_index :items, :big_category_id
-    remove_foreign_key :items, :users
     remove_reference :items, :middle_category
-    remove_index :items, :middle_category_id
   end
 end
--- db/migrate/20190323042223_drop_small_category_from_item.rb
+++ db/migrate/20190323042223_drop_small_category_from_item.rb
@@ -1,7 +1,5 @@
 class DropSmallCategoryFromItem < ActiveRecord::Migration[5.0]
   def change
-    remove_foreign_key :items, :users
     remove_reference :items, :small_category
-    remove_index :items, :small_category_id
   end
 end
