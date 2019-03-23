e3975aedbadc95c7587c8f6f7748ec092a956075
wish911wish <wish911wish@gmail.com>
2019-03-23 14:23:54 +0900

    Edit Item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,24 @@
 class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    binding.pry
+    @item.save
+    redirect_to root_path
+    # render :new unless @item.save
+  end
+
+  private
+  def set_item
+    @item = EventDecorator.find(params[:id]).decorate
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description ,:big_category_id , :middle_category_id, :small_category_id, :brand_id, :size_id, :condition_id, :shipping_cost_id, :shipping_method_id, :sender_prefecture, :days_for_shipment_id, :status_id, :price).merge(seller_id: current_user.id)
+  end
+
 end
