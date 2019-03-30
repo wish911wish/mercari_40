af34f1b3c2b8a9540a97fb92a09860fcad5ae9b8
大庭正考 <oobatadashikou@MacBook-Pro.local>
2019-03-30 12:01:32 +0900

    Add login function using google and facebook account
    Some of code cannot hehave as a role, so I made some sesson variables.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users, controllers: {registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root 'top#index'
   resources :items, only: [:index, :new]
   resources :identification, only: [:index]
   resources :card, only: [:index, :new, :show]
   resources :purchase, only: [:index]
+  resources :signin, only: [:index]
 
   get '/signup', to: 'signup#index', as: 'user_signup'
   get '/signup/sms_confirmation', to: 'signup#sms_confirmation', as: 'sms_confirmation'
   patch '/signup/address_registration', to: 'signup#address_registration', as: 'address_registration'
   patch '/signup/payment_registration', to: 'signup#payment_registration', as: 'payment_registration'
   get '/signup/completion', to: 'signup#completion', as: 'completion'
+
+  get '/auth/:provider/callback',    to: 'users#create',       as: :auth_callback
+  get '/auth/failure',               to: 'users#auth_failure', as: :auth_failure
 
 end
