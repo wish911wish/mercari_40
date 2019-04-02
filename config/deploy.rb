1dcb5c033971cbaaca251e6a41101bf151499ae1
wish911wish <wish911wish@gmail.com>
2019-04-02 21:24:21 +0900

    Comment out of linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url,  'git@github.com:wish911wish/mercari_40.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, %w{ config/secrets.yml }
-set :linked_files, %w{ .env }
+# set :linked_files, %w{ .env }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
