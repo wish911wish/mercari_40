38a1cbe1e009e200fd5b6efd1a041a8fbe837ce4
wish911wish <wish911wish@gmail.com>
2019-04-06 16:14:43 +0900

    Implementation of validation for item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,24 @@ class Item < ApplicationRecord
   has_one :shipping_method
   has_one :days_for_shipment
   has_one :status
-  has_many :item_images, dependent: :destroy
+  has_many :item_images
   accepts_nested_attributes_for :item_images
 
   with_options presence: true do
     validates :name
     validates :description
+    validates :big_category_id
+    validates :middle_category_id
+    validates :small_category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :shipping_method_id
+    validates :sender_prefecture
+    validates :days_for_shipment_id
+    validates :status_id
+    validates :price
   end
 
+  validates :description, length: { maximum: 40 }
+  validates :price ,numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 end
