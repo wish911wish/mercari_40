6dfa5f71140ac8ce5be29f2204314e5d2adc563c
大庭正考 <oobatadashikou@MacBook-Pro.local>
2019-04-17 00:03:55 +0900

    Show user image
    Edit some files to show user image.
    if there isn't any image, default image which has already ready is shown.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
 
   def index
+    user = User.find(current_user.id)
+    if user.image.nil?
+      @user_image = "member_photo_noimage_thumb.png"
+    else
+      @user_image = user.image
+    end
   end
 
   def profile
