974c8ac836c9b8cbebf994b1cdc75743d09f0193
wish911wish <wish911wish@gmail.com>
2019-04-21 02:35:28 +0900

    Implementation of detail search function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -58,6 +58,55 @@ class ItemsController < ApplicationController
     @item = Item.all
   end
 
+  def detail_search
+    search_condition = {}
+
+    if params[:small_category_id].present?
+      search_condition.merge!({small_category_id: params[:small_category_id].keys})
+    end
+
+    if params[:brand_name].present?
+      brand = Brand.find_by(name: params[:brand_name])
+      search_condition.merge!({brand_id: brand.id}) if brand.present?
+    end
+
+    if params[:size_id].present?
+      search_condition.merge!({size_id: params[:size_id].keys})
+    end
+
+    if params[:condition_id].present?
+      search_condition.merge!({condition_id: params[:condition_id].keys})
+    end
+
+    if params[:shipping_cost_id].present?
+      search_condition.merge!({shipping_cost_id: params[:shipping_cost_id].keys})
+    end
+
+    if params[:status_id].present?
+      search_condition.merge!({status_id: params[:status_id].keys})
+    end
+
+    if params[:price_min].present? && params[:price_max].present? && params[:price_min] <= params[:price_max]
+      search_condition.merge!({price: params[:price_min]..params[:price_max]})
+      @item = Item.where(search_condition)
+    elsif params[:price].present?
+      @item = Item.where(search_condition).where(get_price_range)
+    else
+      @item = Item.where(search_condition)
+    end
+
+    if params[:keyword].present?
+      search_with_keyword
+    end
+
+    if params[:sort_order] === "favorites"
+      @item = @item.joins(:favorites).group(:item_id).order('count(item_id) desc')
+    else
+      @item = @item.order(params[:sort_order])
+    end
+    render :search
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :description ,:big_category_id , :middle_category_id, :small_category_id, :size_id, :condition_id, :shipping_cost_id, :shipping_method_id, :sender_prefecture, :days_for_shipment_id, :status_id, :price, item_images_attributes: [:image, :id]).merge(seller_id: current_user.id)
@@ -69,5 +118,21 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def get_price_range
+    prices = params[:price].split("-")
+    if prices.length === 2
+      "price BETWEEN #{prices[0]} AND #{prices[1]}"
+    else
+      "price >= #{prices[0]}"
+    end
+  end
+
+  def search_with_keyword
+    keyword = params[:keyword].split(/[[:blank:]]+/)
+    keyword.each do |keyword|
+      @item = @item.where("name LIKE ?", "%#{keyword}%").or(Item.where("description LIKE ?", "%#{keyword}%"))
+    end
   end
 end
