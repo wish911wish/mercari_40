47d94c240b1f5e1074fb1feee08f3359828f266f
ouchiryota0308 <ryota.ouchi0308@gmail.com>
2019-04-20 13:49:30 +0900

    2nd commit
    Edited top page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,15 @@
 class TopController < ApplicationController
   def index
     @items = Item.all
+
+    @items_ladies = Item.where(big_category_id: "1").limit(4)
+    @section_name_ladies = Category.find(@items_ladies[0].big_category_id).name
+
+    @items_mens = Item.where(big_category_id: "2").limit(4)
+    @section_name_mens = Category.find(@items_mens[0].big_category_id).name
+
+    @items_kids = Item.where(big_category_id: "3").limit(4)
+    @section_name_kids = Category.find(@items_kids[0].big_category_id).name
+
   end
 end
