f01a880bf0f3f86b1d977c2875afc62b9bcdd18d
wish911wish <wish911wish@gmail.com>
2019-04-21 08:10:47 +0900

    Delete unnecessary factory

--- spec/factories/conditions.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :condition do
-    name  { "新品、未使用" }
-  end
-end
--- spec/factories/days_for_shipments.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :days_for_shipment do
-    name  {"1~2日で発送"}
-  end
-end
