eb3984dd878dcd84f65e6b247ce5574da17e9a83
ouchiryota0308 <ryota.ouchi0308@gmail.com>
2019-04-20 21:44:31 +0900

    4th commit
    Edited comments.

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -11,5 +11,20 @@ class TopController < ApplicationController
     @items_kids = Item.where(big_category_id: "3").limit(4)
     @section_name_kids = Category.find(@items_kids[0].big_category_id).name
 
+    @items_cosmetics = Item.where(big_category_id: "7").limit(4)
+    @section_name_cosmetics = Category.find(@items_cosmetics[0].big_category_id).name
+
+    @items_chanel = Item.where(brand_id: "47").limit(4)
+    @section_name_chanel = Brand.find(@items_chanel[0].brand_id).name
+
+    @items_vuitton = Item.where(brand_id: "48").limit(4)
+    @section_name_vuitton = Brand.find(@items_vuitton[0].brand_id).name
+
+    @items_supreme = Item.where(brand_id: "49").limit(4)
+    @section_name_supreme = Brand.find(@items_supreme[0].brand_id).name
+
+    @items_nike = Item.where(brand_id: "50").limit(4)
+    @section_name_nike = Brand.find(@items_nike[0].brand_id).name
+
   end
 end
