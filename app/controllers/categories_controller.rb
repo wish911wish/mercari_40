db7239f5221171da5210b3b0bd542806aa9bf0eb
wish911wish <wish911wish@gmail.com>
2019-04-21 20:58:08 +0900

    Delete unnecessary code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,5 +8,4 @@ class CategoriesController < ApplicationController
     category_size = Category.find(params[:id]).category_size
     category_size.present? ? @sizes = Size.where(size_group: category_size.size_id) : @sizes = []
   end
-
 end
