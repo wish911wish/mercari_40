2f1daa1c8f14565004a8a54fa71e4c03aa7ef6cf
大庭正考 <oobatadashikou@MacBook-Pro.local>
2019-04-21 12:47:31 +0900

    remove login user image dlisplay function
    Desplay the default image for user

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,11 +1,5 @@
 class TopController < ApplicationController
   def index
     @items = Item.all
-    user = User.find(current_user.id)
-    if user.image.nil?
-      @user_image = "member_photo_noimage_thumb.png"
-    else
-      @user_image = user.image
-    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    if current_user.present?
-      user = User.find(current_user.id)
-      @user_image = user.image
-    else
-      @user_image = "member_photo_noimage_thumb.png"
-    end
   end
 
   def profile
   end
 
   def logout
-    if current_user.present?
-      user = User.find(current_user.id)
-      @user_image = user.image
-    else
-      @user_image = "member_photo_noimage_thumb.png"
-    end
   end
 
 end
