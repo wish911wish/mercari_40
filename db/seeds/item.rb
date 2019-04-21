91628082b01ad9f0f5a4439d4d476f4a46a7a5f0
wish911wish <wish911wish@gmail.com>
2019-04-21 16:42:56 +0900

    Edit seed file

--- db/seeds/item.rb
+++ db/seeds/item.rb
@@ -4,3 +4,8 @@ csv_data = CSV.read('db/items.csv', headers: true)
 csv_data.each do |data|
   Item.create!(data.to_hash)
 end
+
+csv_data = CSV.read('db/item_images.csv', headers: true)
+csv_data.each do |data|
+  ItemImage.create(data.to_hash)
+end
